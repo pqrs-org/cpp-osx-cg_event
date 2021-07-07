9bbe22430146bb00272c11244ee413537e833f18
Takayama Fumihiko <tekezo@pqrs.org>
2021-07-08 08:38:05 +0900

    Add pqrs::osx::cg_event::mouse

--- example/main.cpp
+++ example/main.cpp
@@ -1,6 +1,15 @@
+#include <chrono>
 #include <iostream>
 #include <pqrs/osx/cg_event.hpp>
+#include <thread>
 
 int main(void) {
+  auto position = pqrs::osx::cg_event::mouse::cursor_position();
+  std::cout << "x " << position.x << " y " << position.y << std::endl;
+
+  pqrs::osx::cg_event::mouse::post_single_click(position, kCGMouseButtonLeft);
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  pqrs::osx::cg_event::mouse::post_double_click(position, kCGMouseButtonLeft);
+
   return 0;
 }
--- include/pqrs/osx/cg_event.hpp
+++ include/pqrs/osx/cg_event.hpp
@@ -6,4 +6,4 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See http://www.boost.org/LICENSE_1_0.txt)
 
-#include "cg_event/dispatcher.hpp"
+#include "cg_event/mouse.hpp"
