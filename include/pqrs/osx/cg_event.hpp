8ddf3771316af8081d21be2f6a2f5f9f31c525a7
Takayama Fumihiko <tekezo@pqrs.org>
2026-03-09 22:11:46 +0900

    http -> https

--- include/pqrs/osx/cg_event.hpp
+++ include/pqrs/osx/cg_event.hpp
@@ -4,6 +4,6 @@
 
 // (C) Copyright Takayama Fumihiko 2021.
 // Distributed under the Boost Software License, Version 1.0.
-// (See http://www.boost.org/LICENSE_1_0.txt)
+// (See https://www.boost.org/LICENSE_1_0.txt)
 
 #include "cg_event/mouse.hpp"
